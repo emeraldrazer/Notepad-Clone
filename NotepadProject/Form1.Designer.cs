7198ecd000ce07a0ffa5921fbdcde4f9d1c7222a
FijiBoi <125161629+emeraldrazer@users.noreply.github.com>
2023-03-06 23:24:32 +0100

    Add files via upload

---
+++ NotepadProject/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace NotepadProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.darkRB = new System.Windows.Forms.RadioButton();
+            this.lightRB = new System.Windows.Forms.RadioButton();
+            this.location = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1131, 583);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1145, 615);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1137, 589);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "File";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Window;
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.darkRB);
+            this.tabPage2.Controls.Add(this.lightRB);
+            this.tabPage2.Controls.Add(this.location);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.openBTN);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1137, 589);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Other";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(41, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Theme";
+            // 
+            // darkRB
+            // 
+            this.darkRB.AutoSize = true;
+            this.darkRB.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.darkRB.Location = new System.Drawing.Point(76, 119);
+            this.darkRB.Name = "darkRB";
+            this.darkRB.Size = new System.Drawing.Size(48, 17);
+            this.darkRB.TabIndex = 5;
+            this.darkRB.TabStop = true;
+            this.darkRB.Text = "Dark";
+            this.darkRB.UseVisualStyleBackColor = true;
+            this.darkRB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.darkRB_MouseDown);
+            // 
+            // lightRB
+            // 
+            this.lightRB.AutoSize = true;
+            this.lightRB.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lightRB.Location = new System.Drawing.Point(8, 119);
+            this.lightRB.Name = "lightRB";
+            this.lightRB.Size = new System.Drawing.Size(50, 17);
+            this.lightRB.TabIndex = 4;
+            this.lightRB.TabStop = true;
+            this.lightRB.Text = "Light";
+            this.lightRB.UseVisualStyleBackColor = true;
+            this.lightRB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lightRB_MouseDown);
+            // 
+            // location
+            // 
+            this.location.AutoSize = true;
+            this.location.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.location.Location = new System.Drawing.Point(105, 36);
+            this.location.Name = "location";
+            this.location.Size = new System.Drawing.Size(47, 13);
+            this.location.TabIndex = 3;
+            this.location.Text = "location";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Current Location: ";
+            // 
+            // openBTN
+            // 
+            this.openBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.openBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.openBTN.Location = new System.Drawing.Point(8, 10);
+            this.openBTN.Name = "openBTN";
+            this.openBTN.Size = new System.Drawing.Size(75, 23);
+            this.openBTN.TabIndex = 1;
+            this.openBTN.Text = "Open File";
+            this.openBTN.UseVisualStyleBackColor = true;
+            this.openBTN.Click += new System.EventHandler(this.openBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(447, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Created By Marko / emeraldrazer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label4.Font = new System.Drawing.Font("Roboto", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 567);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "All rights reserved 2023®";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 615);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Notepad Project";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openBTN;
+        private System.Windows.Forms.Label location;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton darkRB;
+        private System.Windows.Forms.RadioButton lightRB;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NotepadProject/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NotepadProject
+{
+    public partial class Form1 : Form
+    {
+        readonly string defaultfileName = "textFile.txt";
+        readonly string defaultFilePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+        string fullpathAndName = String.Empty;
+        OpenFileDialog dialog = new OpenFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            lightRB.Checked = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SelectFile();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(fullpathAndName))
+            {
+                streamWriter.WriteLine(textBox1.Text);
+            }
+        }
+
+        private void openBTN_Click(object sender, EventArgs e)
+        {
+            SelectFile();
+        }
+
+        public void SelectFile()
+        {
+            dialog.ShowDialog();
+
+            if (dialog.FileName == String.Empty)
+            {
+                fullpathAndName = String.Concat(defaultFilePath, "\\", defaultfileName);
+                MessageBox.Show($"The file will be stored at {fullpathAndName}", "Notepad Project", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                fullpathAndName = dialog.FileName;
+            }
+
+            location.Text = fullpathAndName;
+
+            try
+            {
+                string readFromFile = File.ReadAllText(fullpathAndName);
+                textBox1.Text = readFromFile;
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void darkRB_MouseDown(object sender, MouseEventArgs e)
+        {
+            tabPage2.ForeColor = System.Drawing.ColorTranslator.FromHtml("#E6E4E4");
+            tabPage2.BackColor = System.Drawing.ColorTranslator.FromHtml("#343434");
+            openBTN.ForeColor = System.Drawing.ColorTranslator.FromHtml("#E6E4E4");
+            openBTN.BackColor = System.Drawing.ColorTranslator.FromHtml("#3d3d3d");
+
+            textBox1.ForeColor = System.Drawing.ColorTranslator.FromHtml("#E6E4E4");
+            textBox1.BackColor = System.Drawing.ColorTranslator.FromHtml("#343434");
+        }
+
+        private void lightRB_MouseDown(object sender, MouseEventArgs e)
+        {
+            tabPage2.ForeColor = SystemColors.WindowText;
+            tabPage2.BackColor = SystemColors.Window;
+            openBTN.ForeColor = SystemColors.WindowText;
+            openBTN.BackColor = SystemColors.Window;
+
+            textBox1.ForeColor = SystemColors.WindowText;
+            textBox1.BackColor = SystemColors.Window;
+        }
+    }
+}
